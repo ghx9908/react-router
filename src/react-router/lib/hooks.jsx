0320313bec66d4c91760e68f113239c29ed95386
gaohongxiang <2460013249@qq.com>
2023-06-29 16:15:55 +0800

    feat: 增加注释

--- src/react-router/lib/hooks.jsx
+++ src/react-router/lib/hooks.jsx
@@ -1,51 +1,54 @@
 import React from "react"
 import { LocationContext, NavigationContext, RouteContext } from "./context"
 import { matchRoutes } from "../../router"
+
+// 获取 LocationContext 中的 location 对象
 export function useLocation() {
   const location = React.useContext(LocationContext)
   return location
 }
-/**
- * 用当前的路径和routes里面的path进行匹配,如果匹配上就渲染当前的element
- * @param {*} routes
- */
+
+// 根据 routes 配置的路由表匹配当前 location,并渲染匹配的路由元素
 export function useRoutes(routes) {
-  const location = useLocation()
-  let pathname = location.pathname || "/"
-  let matches = matchRoutes(routes, { pathname })
+  const location = useLocation() // 获取当前 location 对象
+  let pathname = location.pathname || "/" // 取 pathname,如果不存在则默认为 /
+  let matches = matchRoutes(routes, { pathname }) // 使用 pathname 匹配 routes 路由表
   console.log("matches=>", matches)
-  if (matches) return renderMatches(matches)
+  if (matches) return renderMatches(matches) // 匹配成功则渲染路由组件
 }
 
-// 导出一个名为 renderMatches 的函数，参数为 renderedMatches
+// 从后向前渲染 matches 中匹配的路由元素
 export function renderMatches(renderedMatches) {
-  // 使用 reduceRight 方法从右到左遍历 renderedMatches 数组，累计器初始值为 null
   return renderedMatches.reduceRight((outlet, match, index) => {
-    // 获取当前元素之前的子数组，包含当前元素
     let matches = renderedMatches.slice(0, index + 1)
-    // 返回一个 RouteContext.Provider 组件，将累计值和子数组作为值传递给组件
     return (
       <RouteContext.Provider value={{ outlet, matches }}>
+        {/* 渲染当前匹配的路由组件 */}
         {match.route.element}
       </RouteContext.Provider>
     )
   }, null)
 }
+
+// 返回 react-router 的 navigate 函数
 export function useNavigate() {
   let navigator = React.useContext(NavigationContext)
   let navigate = React.useCallback(
     (to, state) => {
-      navigator.push(to, state)
+      navigator.push(to, state) // 调用 react-router 的 navigator.push 方法
     },
     [navigator]
   )
   return navigate
 }
 
+// 获取当前 Route 的 outlet
 export function useOutlet() {
   let { outlet } = React.useContext(RouteContext)
   return outlet
 }
+
+// 获取当前 Route 的 params 对象
 export function useParams() {
   let { matches } = React.useContext(RouteContext)
   let routeMatch = matches[matches.length - 1]
