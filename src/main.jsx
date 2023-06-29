2877799248b04b8f96a86424a1ff3a73323bd710
gaohongxiang <2460013249@qq.com>
2023-06-29 16:30:35 +0800

    feat: navlink的实现

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import {
   Routes,
   Route,
   Link,
+  NavLink,
 } from "./react-router-dom"
 import Home from "./components/Home"
 import User from "./components/User"
@@ -12,17 +13,29 @@ import Profile from "./components/Profile"
 import UserAdd from "./components/UserAdd"
 import UserList from "./components/UserList"
 import UserDetail from "./components/UserDetail"
+const activeStyle = { backgroundColor: "green" }
+const activeClassName = "active"
+const activeNavProps = {
+  style: ({ isActive }) => (isActive ? activeStyle : {}),
+  className: ({ isActive }) => (isActive ? activeClassName : ""),
+}
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ul>
       <li>
-        <Link to="/">首页</Link>
+        <NavLink end={true} to="/" {...activeNavProps}>
+          首页
+        </NavLink>
       </li>
       <li>
-        <Link to="/user">用户管理</Link>
+        <NavLink to="/user" {...activeNavProps}>
+          用户管理
+        </NavLink>
       </li>
       <li>
-        <Link to="/profile">个人中心</Link>
+        <NavLink to="/profile" {...activeNavProps}>
+          个人中心
+        </NavLink>
       </li>
     </ul>
     <Routes>
--- src/react-router-dom/index.jsx
+++ src/react-router-dom/index.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-children-prop */
+/* eslint-disable react/prop-types */
 import { useState, useLayoutEffect, useRef } from "react"
 import {
   Router,
@@ -70,4 +72,48 @@ export const Link = function (props) {
     navigate(to, state)
   }
   return <a {...rest} onClick={handleClick} />
+}
+
+/**
+ * @param {*} className 类名 可以是固定的字符串，也可以是一个函数，函数的参数是isActive
+ * @param {*} end 是否结束
+ * @param {*} style 行内样式 可以是固定的字符串，也可以是一个函数，函数的参数是isActive
+ * @param {*} to 点击导航跳转的路径
+ * @param {*} children 子组件
+ */
+export function NavLink({
+  className: classNameProp = "",
+  end = false,
+  style: styleProp = {},
+  to,
+  children,
+  ...rest
+}) {
+  let location = useLocation()
+  let path = { pathname: to }
+  let locationPathname = location.pathname //当前的路径
+  let toPathname = path.pathname //当前导航想要跳转的路径
+  //如果路径一样，或者 不结束，并且当前的路径是以to开头的，并且下一个字符/，也就是路径路径分隔符
+  let isActive =
+    locationPathname === toPathname ||
+    (!end &&
+      locationPathname.startsWith(toPathname) &&
+      locationPathname.charAt(toPathname.length) === "/")
+  let className
+  if (typeof classNameProp === "function") {
+    className = classNameProp({
+      isActive,
+    })
+  }
+  let style
+  if (typeof styleProp === "function") {
+    style = styleProp({
+      isActive,
+    })
+  }
+  return (
+    <Link {...rest} to={to} className={className} style={style}>
+      {children}
+    </Link>
+  )
 }
